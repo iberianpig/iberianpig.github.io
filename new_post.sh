be7b849515ff4ba4302d5409f19227b548c57a29
iberianpig <yhkyky@gmail.com>
2017-01-23 22:14:26 +0900

    記事生成スクリプトを配置

---
+++ new_post.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo -e "\033[0;32mCreating new post...\033[0m"
+
+if [ $# -eq 1 ]; then
+  title="_$1"
+else
+  title=""
+fi
+
+formatted_date=$(date "+%Y-%m-%d")
+
+full_path="posts/${formatted_date}${title}.md" 
+
+hugo new "$full_path"
+
+vi "content/$full_path"
